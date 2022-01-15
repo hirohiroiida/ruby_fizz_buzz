16921e3d949eb60815e9dd532c5afffd877c6486
Hピエトロ <awpak1208h@gmail.com>
2022-01-15 23:11:01 +0900

    FizzBuzz課題完成

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -3,13 +3,19 @@ require_relative '../lib/fizz_buzz'
 
 class FizzBuzzTest < Minitest::Test
   def test_fizz_buzz
-    assert_equal '1', fizz_buzz(1)
-    assert_equal '2', fizz_buzz(2)
-    assert_equal 'Fizz', fizz_buzz(3)
-    assert_equal '4', fizz_buzz(4)
-    assert_equal 'Buzz', fizz_buzz(5)
-    assert_equal 'Fizz', fizz_buzz(6)
-    assert_equal 'Fizz Buzz', fizz_buzz(15)
-    assert_equal 'Fizz Buzz', fizz_buzz(150)
+    
+    puts "数字を入れてください"
+    number = gets.to_i
+    
+    if number%15==0
+      puts "fizz_buzz"  
+    elsif number%5==0
+      puts "Buzz"
+    elsif number%3==0
+      puts "Fizz"
+    else
+      puts number
+    end
+
   end
 end
