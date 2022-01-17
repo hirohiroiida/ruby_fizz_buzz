88d04d76ce260400adf5406564d1e616fe292a20
Hピエトロ <awpak1208h@gmail.com>
2022-01-17 19:22:43 +0900

    fizz_buzz.rbにやり直し

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,3 +1,15 @@
 def fizz_buzz(number)
-  # 実装してください
+  if number % 15 == 0
+    puts 'fizz_buzz'
+  elsif number % 5 == 0
+    puts 'Buzz'
+  elsif number % 3 == 0
+    puts 'Fizz'
+  else
+    puts number
+  end
 end
+
+puts '半角で数字を入れてください？'
+number = gets.to_i
+fizz_buzz(number)
--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -1,21 +1,14 @@
 require 'minitest/autorun'
 require_relative '../lib/fizz_buzz'
 
-class FizzBuzzTest < Minitest::Test
-  def test_fizz_buzz
-    
-    puts "数字を入れてください"
-    number = gets.to_i
-    
-    if number%15==0
-      puts "fizz_buzz"  
-    elsif number%5==0
-      puts "Buzz"
-    elsif number%3==0
-      puts "Fizz"
-    else
-      puts number
-    end
 
+  def test_fizz_buzz
+    assert_equal '1', fizz_buzz(1)
+    assert_equal '2', fizz_buzz(2)
+    assert_equal 'Fizz', fizz_buzz(3)
+    assert_equal '4', fizz_buzz(4)
+    assert_equal 'Buzz', fizz_buzz(5)
+    assert_equal 'Fizz', fizz_buzz(6)
+    assert_equal 'Fizz Buzz', fizz_buzz(15)
+    assert_equal 'Fizz Buzz', fizz_buzz(150)
   end
-end
